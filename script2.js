1452524920071f815bcb7c16cc6250971aacfc19
Marina Rull Valdivieso <marinarullvaldivieso@Marinas-MacBook-Pro-26.local>
2023-10-04 10:37:00 +0200

    nuevos docs

---
+++ script2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Muerte</title>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+   
+    <div class="contenido">
+
+    </div>
+    
+</body>
+</html>
